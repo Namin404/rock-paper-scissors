277d87a4399ca0de087dc3e6b049012290b11648
Namin <namin22@gmail.com>
2022-07-11 17:40:44 -0300

    Made new javascript file, restarted assignment with cleaner code

--- script.js
+++ script.js
@@ -1,46 +1,30 @@
-// function computerPlay("Rock", "Paper", "Scissors") {
 
-// }
-
-console.log("Is this working?")
-
-
-/* function computerPlay() {
-    const myArray = ["Rock", "Paper", "Scissors"]
-    const random = [Math.floor(Math.random() * myArray.length)]
-    console.log(random)
-} */
-
-
-const months = ["January", "February", "March", "April", "May", "June", "July"];
-
-const random = Math.floor(Math.random() * months.length);
-console.log(months[random]);
+/* Your game is going to play against the computer, so begin with a
+function called computerPlay that will randomly return either
+‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to
+make the computer’s play. Tip: use the console to make sure this is
+returning the expected output before moving to the next step! */
 
 function computerPlay() {
-const myArray = ["Rock", "Paper", "Scissors"]
-const random2 = [Math.floor(Math.random() * myArray.length)]
-console.log(myArray[random2])
-}
+    const myArray = ["Rock", "Paper", "Scissors"]
+    const random2 = [Math.floor(Math.random() * myArray.length)]
+    return myArray[random2];
+    }
 
-computerPlay() 
+let computerSelection = computerPlay();
+console.log(computerSelection)
 
-
+/*
 function playerPlay() {
     const myArray = ["Rock", "Paper", "Scissors"]
     const random2 = [Math.floor(Math.random() * myArray.length)]
     console.log(myArray[random2])
     }
 
-playerPlay()
+    playerPlay()
 
-/* else if (computerPlay === playerPlay) {
-    return "It's a tie!";
-} */
-
-
-/* function Winner() {
-    if ((computerPlay === "Paper") && (playerPlay === "Rock")) {
+function Winner() {
+    if (computerPlay === "Paper" && playerPlay === "Rock") {
         return "You lose! Paper beats rock";
     }
     else if (computerPlay === "Rock" && playerPlay === "Scissors") {
